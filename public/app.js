060fadf281f695c0a4bf90a9b68d7f45692630af
Duane McFarlane <duane.mcfar@gmail.com>
2021-09-27 10:31:45 -0400

    removed sandbox, created app.ts

---
+++ public/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// const anchor = document.querySelector('a')!;
+// console.log(anchor.href)
+// const 
+var form = document.querySelector('.new-item-form'); // type casting
+console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#tofrom');
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// const anchor = document.querySelector('a')!;
+// console.log(anchor.href)
+
+// const 
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement; // type casting
+
+console.log(form.children)
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLSelectElement;
+
