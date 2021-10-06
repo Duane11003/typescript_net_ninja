6359b1b9ca4afe17a0a55a82dd67015285a8889b
Duane McFarlane <duanemcfarlane@Duanes-MacBook-Pro.local>
2021-10-06 19:23:31 -0400

    generics

--- public/app.js
+++ public/app.js
@@ -1,20 +1,6 @@
 import { Invoice } from './classes/invoice.js';
+import { ListTemplate } from './classes/listTemplate.js';
 import { Payment } from './classes/payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'webwork', 250)
-// docTwo = new Payment('mario', 'plumb work', 200)
-// let docs: HasFormatter[] = [];
-// docs.push(docOne)
-// docs.push(docTwo)
-// console.log('docs....', docs)
-// const invoiceOne = new Invoice('mario', 'work on mario website', 400)
-// const invoiceTwo = new Invoice('luigi', 'work on luigi website', 300)
-// let invoices:Invoice[] = []; // only invoices can be added to this array
-// invoices.push(invoiceOne, invoiceOne)
-// readonly: only allowed to read it, not change it, inside and outside the class
-// private: only allowed to access it directly within the class. can access it if its part of a method
-// public: allowed to access and change it wherever
 // form
 const form = document.querySelector('.new-item-form'); // type casting
 // inputs
@@ -22,6 +8,8 @@ const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -31,5 +19,23 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log('doc...', doc);
+    list.render(doc, type.value, 'end');
 });
+// generics: allows us to create reusable blocks of code
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 1000);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'duane', age: 40 });
+console.log(docOne);
+const docThree = {
+    uuid: 3,
+    resourceName: 'duane',
+    data: 'john'
+};
+const docFour = {
+    uuid: 5,
+    resourceName: 'gianna',
+    data: ['test']
+};
+const greeting = (name) => `hi, i'm ${name}`;
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 
 import { Invoice } from './classes/invoice.js'
+import { ListTemplate } from './classes/listTemplate.js';
 import { Payment } from './classes/payment.js'
 import { HasFormatter } from './interfaces/HasFormatter.js'
 
@@ -12,30 +13,6 @@ interface IsPerson {
     spend(a: number): number;
 }
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'webwork', 250)
-// docTwo = new Payment('mario', 'plumb work', 200)
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne)
-// docs.push(docTwo)
-
-// console.log('docs....', docs)
-
-// const invoiceOne = new Invoice('mario', 'work on mario website', 400)
-// const invoiceTwo = new Invoice('luigi', 'work on luigi website', 300)
-
-// let invoices:Invoice[] = []; // only invoices can be added to this array
-
-// invoices.push(invoiceOne, invoiceOne)
-
-// readonly: only allowed to read it, not change it, inside and outside the class
-// private: only allowed to access it directly within the class. can access it if its part of a method
-// public: allowed to access and change it wherever
-
-
 // form
 const form = document.querySelector('.new-item-form') as HTMLFormElement; // type casting
 
@@ -44,6 +21,9 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#tofrom') as HTMLSelectElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!
+const list = new ListTemplate(ul)
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
@@ -54,5 +34,37 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
     }
 
-    console.log('doc...', doc)
+    list.render(doc, type.value, 'end')
+
 })
+
+
+// generics: allows us to create reusable blocks of code
+
+const addUID = <T extends {name: string}>(obj: T) => { // the <T> captures whatever properties we pass into the function
+ let uid = Math.floor(Math.random() * 1000)
+ return {...obj, uid}
+}
+
+let docOne = addUID({name: 'duane', age: 40})
+console.log(docOne)
+
+interface Resource<T> { // reminder: interfaces define how objects should look
+    uuid: number;
+    resourceName: string;
+    data: T
+}
+
+const docThree: Resource<string> = {
+    uuid: 3,
+    resourceName: 'duane',
+    data: 'john'
+}
+
+const docFour: Resource<string[]> = {
+    uuid: 5,
+    resourceName: 'gianna',
+    data: ['test']
+}
+
+const greeting = (name: string) => `hi, i'm ${name}`
